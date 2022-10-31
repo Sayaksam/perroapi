843efc655a92791893c5aa5ccb187628391b780c
Sayaksam <32608668+Sayaksam@users.noreply.github.com>
2022-10-31 23:14:35 +0530

    Add files via upload

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+"""Simple add file"""
+
+
+def add(x, y):
+    return x + y
+
+
+def substract(x, y):
+    if x > y:
+        return x - y
+    elif y > x:
+        return y - x
+    else:
+        return 0
